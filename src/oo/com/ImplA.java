dccd994ca6d551e9acbcca74f6876e0408e62d77
Max.chen <max.chen@base.tw>
2013-04-03 11:24:06 +0800

    commit測試中

---
+++ src/oo/com/ImplA.java
@@ -0,0 +1,13 @@
+package oo.com;
+
+public class ImplA implements Api {
+
+	@Override
+	public void operation(String s) {
+		// TODO Auto-generated method stub
+		System.out.println("簡單工廠一"+s);
+		
+	}
+	
+	
+}
